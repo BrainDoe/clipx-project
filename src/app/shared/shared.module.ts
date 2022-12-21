b98cf166ebe96db5b5510a0c636e5bce231f2283
Donatus Brain Okwe <braindona@gmail.com>
2022-12-21 09:59:17 -0600

    Completed the modal. Started working on the tabs

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './components/modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabsComponent } from './tabs/tabs.component';
+
+
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [ModalComponent, TabsContainerComponent, TabsComponent]
+})
+export class SharedModule { }
